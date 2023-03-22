a76a88375ea108b8898b7994c35e4fd1498942a4
EmilieGanmont <77340577+EmilieGanmont@users.noreply.github.com>
2023-03-22 11:40:09 -0400

    Player Movement

---
+++ Pair-Optimization-Game/Assets/Scripts/Player Input Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private PlayerControls playercontrols;
+    private InputAction movement;
+
+    [SerializeField] private float movementSpeed = 1f;
+    private Rigidbody2D rg2d;
+
+    private void Awake()
+    {
+        playercontrols = new PlayerControls();
+        rg2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnEnable()
+    {
+        movement = playercontrols.PlayerMovement.Movement;
+        movement.Enable();
+    }
+
+    private void FixedUpdate()
+    {
+        rg2d.velocity = movement.ReadValue<Vector2>() * movementSpeed;
+    }
+}
